26f5716f85490f6f8db9db774664e60db5b8e9b7
V <2583695112@qq.com>
2022-03-18 17:30:59 +0800

    eslint+prettier and rem适配

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: true,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  parser: "babylon",
+};
